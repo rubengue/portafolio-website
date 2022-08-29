ac8e3770cbcedef8ffcc2546951b6c9f0c196690
Ruben Guevara <rubenguev@gmail.com>
2022-08-28 21:29:45 -0500

    adding changes

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -1,8 +1,98 @@
 import React from 'react'
+import './experience.css'
+import {BsFillPatchCheckFill} from 'react-icons/bs'
 
 const Experience = () => {
   return (
-    <section id='experience'>Experience</section>
+    <section id='experience'>
+      <h5>What Skills I Have</h5>
+      <h2>My Experience</h2>
+
+      <div className='container experience__container'>
+        <div className='experience__devops'>
+          <h3>Devops Engineer</h3>
+          <div className='experience__content'>
+            <article className='experience__details'>
+              <BsFillPatchCheckFill className='experience__details-icon'/>
+              <div>
+                <h4>Kubernetes</h4> 
+                <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+            <article className='experience__details'>
+              <BsFillPatchCheckFill className='experience__details-icon'/>
+              <div>
+                <h4>Pipelines</h4> 
+                <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+            <article className='experience__details'>
+              <BsFillPatchCheckFill className='experience__details-icon'/>
+              <div>
+                <h4>Docker</h4> 
+                <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+            <article className='experience__details'>
+              <BsFillPatchCheckFill className='experience__details-icon'/>
+              <div>
+                <h4>CI/CD</h4> 
+                <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+            <article className='experience__details'>
+              <BsFillPatchCheckFill className='experience__details-icon'/>
+              <div>
+                <h4>Gitflow</h4> 
+                <small className='text-light'>Intermediate</small>
+              </div>
+            </article>
+          </div>
+        </div>
+        {/* END OF DEVOPS */}
+
+        <div className='experience__cloud-arquitecture'>
+        <h3>Cloud Computing</h3>
+          <div className='experience__content'>
+            <article className='experience__details'>
+              <BsFillPatchCheckFill className='experience__details-icon'/>
+              <div>
+                <h4>Amazon AWS</h4> 
+                <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+            <article className='experience__details'>
+              <BsFillPatchCheckFill className='experience__details-icon'/>
+              <div>
+                <h4>Google GCP</h4> 
+                <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+            <article className='experience__details'>
+              <BsFillPatchCheckFill className='experience__details-icon'/>
+              <div>
+                <h4>Microsoft Azure</h4> 
+                <small className='text-light'>Intermediate</small>
+              </div> 
+            </article>
+            <article className='experience__details'>
+              <BsFillPatchCheckFill className='experience__details-icon'/>
+              <div>
+                <h4>Cloudflare</h4> 
+                <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+            <article className='experience__details'>
+              <BsFillPatchCheckFill className='experience__details-icon'/>
+              <div>
+                <h4>Github</h4> 
+                <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+          </div>
+      </div>
+      </div>
+    </section>
   )
 }
 
--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,8 +1,144 @@
 import React from 'react'
+import './services.css'
+import {BiCheck} from 'react-icons/bi'
 
 const Services = () => {
   return (
-    <section id='services'>Services</section>
+    <section id='services'>
+      <h5>What I Offer</h5>
+      <h2>Services</h2>
+
+      <div container className="container services__container">
+        <article className='service'>
+          <div className='service__head'>
+            <h3>CI/CD Pipelines design</h3>
+          </div>
+
+          <ul className='service__list'>
+            <li>
+              <BiCheck className='service__list-icon'/>
+              <p>Lorem ipsum, dolor sit amet consectur elit</p>
+            </li>
+
+            <li>
+              <BiCheck className='service__list-icon'/>
+              <p>Lorem ipsum, dolor sit amet consectur elit</p>
+            </li>
+
+            <li>
+              <BiCheck className='service__list-icon'/>
+              <p>Lorem ipsum, dolor sit amet consectur elit</p>
+            </li>
+
+            <li>
+              <BiCheck className='service__list-icon'/>
+              <p>Lorem ipsum, dolor sit amet consectur elit</p>
+            </li>
+
+            <li>
+              <BiCheck className='service__list-icon'/>
+              <p>Lorem ipsum, dolor sit amet consectur elit</p>
+            </li>
+
+            <li>
+              <BiCheck className='service__list-icon'/>
+              <p>Lorem ipsum, dolor sit amet consectur elit</p>
+            </li>
+
+          </ul>
+        </article>
+
+        {/* END OF PIPELINES */}
+
+        <article className='service'>
+          <div className='service__head'>
+            <h3>Cloud Infra Management</h3>
+          </div>
+
+          <ul className='service__list'>
+            <li>
+              <BiCheck className='service__list-icon'/>
+              <p>Lorem ipsum, dolor sit amet consectur elit</p>
+            </li>
+
+            <li>
+              <BiCheck className='service__list-icon'/>
+              <p>Lorem ipsum, dolor sit amet lol consectur elit</p>
+            </li>
+
+            <li>
+              <BiCheck className='service__list-icon'/>
+              <p>Lorem ipsum, dolor sit amet consectur elit</p>
+            </li>
+
+            <li>
+              <BiCheck className='service__list-icon'/>
+              <p>Lorem ipsum, dolor sit amet consectur elit</p>
+            </li>
+
+            <li>
+              <BiCheck className='service__list-icon'/>
+              <p>Lorem ipsum, dolor sit amet consectur elit</p>
+            </li>
+
+            <li>
+              <BiCheck className='service__list-icon'/>
+              <p>Lorem ipsum, dolor sit amet consectur elit</p>
+            </li>
+
+            <li>
+              <BiCheck className='service__list-icon'/>
+              <p>Lorem ipsum, dolor sit amet consectur elit</p>
+            </li>
+
+          </ul>
+        </article>
+        
+        {/* END OF INFRA DESIGN */}
+
+        <article className='service'>
+          <div className='service__head'>
+            <h3>Infrastructure Arquitecture</h3>
+          </div>
+
+          <ul className='service__list'>
+            <li>
+              <BiCheck className='service__list-icon'/>
+              <p>Lorem ipsum, dolor sit amet consectur elit</p>
+            </li>
+
+            <li>
+              <BiCheck className='service__list-icon'/>
+              <p>Lorem ipsum, dolor sit amet consectur elit</p>
+            </li>
+
+            <li>
+              <BiCheck className='service__list-icon'/>
+              <p>Lorem ipsum, dolor sit amet consectur elit</p>
+            </li>
+
+            <li>
+              <BiCheck className='service__list-icon'/>
+              <p>Lorem ipsum, dolor sit amet consectur elit</p>
+            </li>
+
+            <li>
+              <BiCheck className='service__list-icon'/>
+              <p>Lorem ipsum, dolor sit amet consectur elit</p>
+            </li>
+
+            <li>
+              <BiCheck className='service__list-icon'/>
+              <p>Lorem ipsum, dolor sit amet consectur elit</p>
+            </li>
+
+          </ul>
+        </article>
+        
+        {/* END OF PIPELINES */}
+
+      </div>
+    </section>
   )
 }
 
